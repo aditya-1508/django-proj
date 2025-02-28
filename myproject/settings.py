bd4bc4a67b5603f34f5b50b9fcb9451fd5e6b9e8
aditya-1508 <aditya.chandrakar@ssipmt.com>
2025-02-28 23:15:52 +0530

    Initial commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h=fu8djm7bwukqo1juvy-+zpw(b3%=#cx5$(=6vnq!a#@cd&^o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
